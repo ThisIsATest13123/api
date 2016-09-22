e27891d628fecc3a9462b798d9f6c4eab2e49fe0
Yash Kumar <ykumar6@gmail.com>
2016-09-22 16:42:24 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,10 @@
-FROM node
-run npm xmas
-cmd npm xmas
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
