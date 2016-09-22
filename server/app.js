58357df496e1f84e109e1812347ca38b645c7664
Yash Kumar <ykumar6@gmail.com>
2016-09-22 16:43:46 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, API!\n');
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:80/');
